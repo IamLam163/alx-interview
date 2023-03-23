54bd7dd6fc78f5b0e0111f0450880b4aca670a42
IamLam163 <olamidealiyu@gmail.com>
2023-03-23 19:57:35 +0100

    linter issues

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,15 +3,16 @@
 Function returns pascal's triangle
 """
 
+
 def pascal_triangle(n):
     """Function for pascal triangle"""
     if n <= 0:
         return []
     triangle = [[1]]
-    for i in range(1, n): #n is another array in triangle
-        row = [1] #every row begins with 1
+    for i in range(1, n):  # n is another array in triangle
+        row = [1]  # every row begins with 1
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
     return triangle
