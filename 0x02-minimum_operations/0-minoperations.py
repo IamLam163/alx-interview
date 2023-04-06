1a9b79fc3e8c84e92967bb42a0ec64f4c2f9fb7e
IamLam163 <olamidealiyu@gmail.com>
2023-04-06 11:44:13 +0100

    more lint fixes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -19,10 +19,3 @@ def minOperations(n):
     if n > 1:  # adds the remaining prime factors of n
         operations += n
     return operations
-
-
-n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
