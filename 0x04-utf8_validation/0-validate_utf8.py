0624fdc61748772f9e1f49c231f3685ae7704b17
IamLam163 <olamidealiyu@gmail.com>
2023-04-19 22:48:11 +0100

    original code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,17 +12,14 @@ def validUTF8(data):
     num_bytes = 0
     for byte in data:
         if num_bytes == 0:
-            if byte >> 7 == 0b0:
-                pass
             if byte >> 5 == 0b110:
                 num_bytes = 1
             elif byte >> 4 == 0b1110:
                 num_bytes = 2
             elif byte >> 3 == 0b11110:
                 num_bytes = 3
-            else:
+            elif byte >> 7:
                 return False
-            # elif byte >> 7:
         else:
             if byte >> 6 != 0b10:
                 return False
