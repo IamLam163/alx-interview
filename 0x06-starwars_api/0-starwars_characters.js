ca3aa404ad0fa98dc7385fbb73ced375b70d4e83
IamLam163 <olamidealiyu@gmail.com>
2023-05-03 17:43:16 +0100

    more bug fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -18,58 +18,24 @@ if (args.length < 2) {
     } else {
       const data = JSON.parse(body);
       const characters = data.characters;
-      characters.forEach(characterUrl => {
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error(error);
-          } else {
-            const characterData = JSON.parse(body);
-            console.log(characterData.name);
-          }
+      const characterNames = characters.map(characterUrl => {
+        return new Promise((resolve, reject) => {
+          request(characterUrl, (error, response, body) => {
+            if (error) {
+              reject(error);
+            } else {
+              const characterData = JSON.parse(body);
+              resolve(characterData.name);
+            }
+          });
         });
+      });
+      Promise.all(characterNames).then(names => {
+        names.forEach(name => console.log(name));
       });
     }
   });
 }
-
-// #!/usr/bin/node
-// /*
-//  * print the list of characters
-//  * */
-//
-// const request = require('request');
-//
-// const url = 'https://swapi.dev/api/films/';
-// const args = process.argv.slice(1);
-//
-// if (args.length < 2) {
-//   console.log('Please provide movie id!');
-// } else {
-//   const movie = args[1];
-//   request(`${url}${movie}`, (error, response, body) => {
-//     if (error) {
-//       console.error(error);
-//     } else {
-//       const data = JSON.parse(body);
-//       const characters = data.characters;
-//       const characterNames = characters.map(characterUrl => {
-//         return new Promise((resolve, reject) => {
-//           request(characterUrl, (error, response, body) => {
-//             if (error) {
-//               reject(error);
-//             } else {
-//               const characterData = JSON.parse(body);
-//               resolve(characterData.name);
-//             }
-//           });
-//         });
-//       });
-//       Promise.all(characterNames).then(names => {
-//         names.forEach(name => console.log(name));
-//       });
-//     }
-//   });
-// }
 
 // const request = require('request');
 //
