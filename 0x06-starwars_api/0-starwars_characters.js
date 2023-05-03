f07bc07c95e6de679326c21db1f4fa87a18be8ac
IamLam163 <olamidealiyu@gmail.com>
2023-05-03 17:15:47 +0100

    used the request module

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,52 +2,69 @@
 /*
  * print the list of characters
  * */
-// fetch('https://swapi.dev/api/people/1/')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(err => console.error(err))
 
+const request = require('request');
 
 const url = 'https://swapi.dev/api/films/';
 const args = process.argv.slice(1);
 
 if (args.length < 2) {
-  console.log('args cannot be less than two!');
+  console.log('Please provide movie id!');
 } else {
   const movie = args[1];
-  fetch(`${url}${movie}`)
-    .then(response => response.json())
-    .then(data => {
+  request(`${url}${movie}`, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const data = JSON.parse(body);
       const characters = data.characters;
       const characterNames = characters.map(characterUrl => {
-        return fetch(characterUrl)
-          .then(response => response.json())
-          .then(CharacterData => CharacterData.name);
+        return new Promise((resolve, reject) => {
+          request(characterUrl, (error, response, body) => {
+            if (error) {
+              reject(error);
+            } else {
+              const characterData = JSON.parse(body);
+              resolve(characterData.name);
+            }
+          });
+        });
       });
       Promise.all(characterNames).then(names => {
         names.forEach(name => console.log(name));
       });
-    })
-    .catch(err => console.error(err));
+    }
+  });
 }
 
-/*
-url = 'https://swapi.dev/api/films/'
-movie = process.argv[2]
-
-fetch(`${url}${movie}`)
-  .then(response => response.json())
-  .then(data => {
-    const characters = data.characters;
-    const characterNames = characters.map(characterUrl => {
-      return fetch(characterUrl)
-        .then(response => response.json())
-        .then(characterData => characterData.name);
-    });
-    Promise.all(characterNames).then(names .then=> {
-      names.forEach(name => console.log(name));
-    });
-  })
-  .catch(err => console.error(err))
-
-*/
+// const request = require('request');
+//
+// const url = 'https://swapi.dev/api/films/';
+// const args = process.argv.slice(1);
+//
+// if (args.length < 2) {
+//   console.log('args cannot be less than two!');
+// } else {
+//   const movie = args[1];
+//   request.get(`${url}${movie}`, (error, response, body) => {
+//     if (error) {
+//       console.error(error);
+//       return;
+//     }
+//     const data = JSON.parse(body);
+//     const characters = data.characters;
+//     const characterNames = characters.map(characterUrl => {
+//       return request.get(characterUrl, (error, response, body) => {
+//         if (error) {
+//           console.error(error);
+//           return;
+//         }
+//         const characterData = JSON.parse(body);
+//         return characterData.name;
+//       });
+//     });
+//     Promise.all(characterNames).then(names => {
+//       names.forEach(name => console.log(name));
+//     });
+//   });
+// }
