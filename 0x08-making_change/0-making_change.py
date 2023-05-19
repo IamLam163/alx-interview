cfafabf9c037317c47cd1d9ef8ef2dcff6c4aa7f
IamLam163 <olamidealiyu@gmail.com>
2023-05-19 04:17:26 +0100

    finding the fewest number of coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values, determine the 
+fewest number of coins needed to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """function determines the fewest number of coins 
+    needed to meet a given amount total."""
+    if total <= 0:
+        return
+    sorted_coins = sorted(coins, reverse=True)
+    coin_counter = 0
+
+    for coin in sorted_coins:
+        while coin <= total:
+            total -= coin
+            coin_counter += 1
+    if total == 0:
+        return coin_counter
+    return -1
+
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
