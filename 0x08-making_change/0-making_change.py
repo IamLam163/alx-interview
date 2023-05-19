7bb14c988d95f4b2a84cb8087ec16cb25218a8f2
IamLam163 <olamidealiyu@gmail.com>
2023-05-19 05:26:17 +0100

    more fixes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,6 +8,21 @@ fewest number of coins needed to meet a given amount total.
 def makeChange(coins, total):
     """function determines the fewest number of coins
     needed to meet a given amount total."""
+    counter = 0
+    if total <= 0:
+        return 0
+    sort = sorted(coins, reverse=True)
+    # for number in sort:
+    for i in range(len(sort)):
+        while sort[i] <= total:
+            total = total - sort[i]
+            counter += 1
+    if total == 0:
+        return counter
+    return -1
+
+
+"""
     if total <= 0:
         return
     sorted_coins = sorted(coins, reverse=True)
@@ -19,4 +34,4 @@ def makeChange(coins, total):
             coin_counter += 1
     if total == 0:
         return coin_counter
-    return -1
+    return -1"""
